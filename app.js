f4b0e1ef5c7512d6fcb478ccd5c8d8bd9937727f
danivanee <danivanee_170@hotmail.com>
2022-12-29 17:17:37 -0600

    se agregaron cheeseburger y hodog como objetos al arreglo de carta

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ const cardArray = [
         name: 'fries',
         img: 'img/fries.png',
     },
+    {
+        name: 'cheeseburger',
+        img: 'img/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'img/hotdog.png',
+    },
+    
 ]
