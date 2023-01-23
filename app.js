c08fb8ce8e93b098dcb6793edd98efe30cc558ca
danivanee <danivanee_170@hotmail.com>
2023-01-23 17:34:59 -0600

    cambios para funcionalidad del juego

--- app.js
+++ app.js
@@ -52,8 +52,9 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector("#grid");
-const cardsChosen = []
-const cardsChosenIds = []
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
 function createBoard() {
   for (let i = 0; i < cardArray.length; i++) {
@@ -73,8 +74,14 @@ console.log(cards)
   if (cardsChosen[0] == cardsChosen[1]){
     alert('You found a match!')
     cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png')
-    
+    cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png')
+    cards[cardsChosenIds[0]].removeEventListener('click', flipCard)
+    cards[cardsChosenIds[1]].removeEventListener('click', flipCard)
+    cardsWon.push(cardsChosen)
   }
+
+  cardsChosen = []
+  cardsChosenIds = []
 }
 
 function flipCard() {
